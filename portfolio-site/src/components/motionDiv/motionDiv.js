4aa4996b1b06a3a264bf9c0e30c119b38daf1bac
gcran <craneg@wwu.edu>
2023-02-17 00:37:03 -0800

    added close button to modal, still need to implement its functionality and add styling, but the positioning is good to go

--- portfolio-site/src/components/motionDiv/motionDiv.js
+++ portfolio-site/src/components/motionDiv/motionDiv.js
@@ -10,35 +10,55 @@ export default function MotionDiv( {data})  {
         setIsAnimationComplete(true);
       }
 
-      useEffect(() => {
-        if (isAnimationComplete) {
-          const element = document.querySelector(".motionContainer .box");
-          element.innerHTML = data;
-        }
-      }, [data, isAnimationComplete]);
+    //   useEffect(() => {
+    //     // if (isAnimationComplete) {
+    //       const element = document.querySelector(".motionContainer .box");
+    //       element.innerHTML = data;
+    //     // }
+    //     if (isAnimationComplete) {
+            
+    //     }
+    //   }, [data, isAnimationComplete]);
 
     return (
         <div className="motionContainer">
-           
-            <motion.div
-                initial={{
-                    rotate: 180,
-              
-                }}
-                className={`box ${isAnimationComplete ? "boxComplete" : ""}`}
-                animate = {{
-                    scale: [0, 1],
-                    rotate: 360,
-                    // borderRadius: '10%'
-                }}
-                transition={{ 
-                    type: "spring", 
-                    damping: 17,
-                    stiffness: 150,
-                    
-                }}
-                onAnimationComplete={handleAnimationComplete}
-            />
+            <div className="motionWrapper">
+                {isAnimationComplete &&( <motion.button className = 'closeModalButton'
+                                                whileHover={{ scale: 1.1 }}
+                                                whileTap={{ scale: 0.9 }}
+                                            >
+                                                Exit
+                                            </motion.button>)}
+                {/* <motion.button className = 'closeModalButton'
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.9 }}
+                >
+                    Exit
+                </motion.button> */}
+
+                <motion.div
+                    initial={{
+                        rotate: 180,             
+                    }}
+                    className={`box ${isAnimationComplete ? "boxComplete" : ""}`}
+             
+                    animate = {{
+                        scale: [0, 1],
+                        rotate: 360,
+                    }}
+                    transition={{ 
+                        type: "spring", 
+                        damping: 25,
+                        stiffness: 300,
+                        
+                    }}
+                    onAnimationComplete={handleAnimationComplete}
+                >
+                
+                   
+                    <div dangerouslySetInnerHTML={{ __html: data }}></div>
+                </motion.div>
+            </div>  
         </div>
     );
 }
