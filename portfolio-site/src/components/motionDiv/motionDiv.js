346d9015cde1defdd0f162e46377fd1ad4646aea
gcran <craneg@wwu.edu>
2023-02-18 02:07:24 -0800

    figured out animated text border issue

--- portfolio-site/src/components/motionDiv/motionDiv.js
+++ portfolio-site/src/components/motionDiv/motionDiv.js
@@ -1,6 +1,24 @@
 import { motion, useCycle } from "framer-motion"
 import './motionDiv.css';
 import { useState, useEffect} from 'react'
+import AnimatedText from "./animatedText";
+
+  // Placeholder text data, as if from API
+  const placeholderText = [
+    { type: "heading1", text: "Framer Motion" },
+    {
+      type: "heading2",
+      text: "Animating responsive text! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    }
+  ];
+
+  const container = {
+    visible: {
+      transition: {
+        staggerChildren: 0.025
+      }
+    }
+  };
 
 export default function MotionDiv( {data})  {
     const rotate = 180;
@@ -56,7 +74,21 @@ export default function MotionDiv( {data})  {
                 >
                 
                    
-                    <div dangerouslySetInnerHTML={{ __html: data }}></div>
+                    {/* <div dangerouslySetInnerHTML={{ __html: data }}></div> */}
+                    <motion.div
+                        className="animatedText"
+                        initial="hidden"
+                        // animate="visible"
+                        animate="visible"
+                        variants={container}
+                        >
+                        <div className="container">
+                            {placeholderText.map((item, index) => {
+                            return <AnimatedText {...item} key={index} />;
+                            })}
+                        </div>
+
+                    </motion.div>
                 </motion.div>
             </div>  
         </div>
